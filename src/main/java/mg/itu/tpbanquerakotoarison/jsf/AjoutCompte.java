fed1b231b25cbc5eb5b40c36e8ef1b6c2e0b0751
DanihStephane <harilantodanih@gmail.com>
2023-04-13 17:45:30 +0300

    Ajout d'un nouveau compte bancaire

---
+++ src/main/java/mg/itu/tpbanquerakotoarison/jsf/AjoutCompte.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquerakotoarison.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.validation.constraints.PositiveOrZero;
+import mg.itu.tpbanquerakotoarison.ejb.GestionnaireCompte;
+import mg.itu.tpbanquerakotoarison.entities.CompteBancaire;
+
+/**
+ * 
+ * @author s.rakotoarison
+ */
+@Named(value = "ajoutCompte")
+@RequestScoped
+public class AjoutCompte {
+
+    @EJB
+    GestionnaireCompte gestionnaireCompte;
+
+    private String nom;
+
+    @PositiveOrZero
+    private int solde;
+
+    /**
+     * Creates a new instance of AjoutCompte
+     */
+    public AjoutCompte() {
+    }
+
+    public String enregistrer() {
+        gestionnaireCompte.creerCompte(new CompteBancaire(nom, solde));
+        Util.addFlashInfoMessage("Un nouveau compte bancaire sous le nom de \"" + nom + "\" a été créé");
+        return "listeComptes?faces-redirect=true";
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+}
