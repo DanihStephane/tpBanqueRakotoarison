879cd4f81c53bc69db8a6c2a67e780f670db3c42
DanihStephane <harilantodanih@gmail.com>
2023-04-13 15:04:48 +0300

    Création de l'EJB session singleton Init

---
+++ src/main/java/mg/itu/tpbanquerakotoarison/ejb/Init.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquerakotoarison.ejb;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.EJB;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+import mg.itu.tpbanquerakotoarison.entities.CompteBancaire;
+
+/**
+ *
+ * @author s.rakotoarison
+ */
+@Singleton
+@Startup
+public class Init {
+
+    @EJB
+    private GestionnaireCompte gc;
+
+    @PostConstruct
+    public void postConstruct() {
+        gc.creerCompte(new CompteBancaire("John Lennon", 150000));
+        gc.creerCompte(new CompteBancaire("Paul McCartney", 950000));
+        gc.creerCompte(new CompteBancaire("Ringo Starr", 20000));
+        gc.creerCompte(new CompteBancaire("Georges Harrisson", 100000));
+    }
+}
